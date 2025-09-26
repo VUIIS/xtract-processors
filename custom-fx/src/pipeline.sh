b6f9b5225547e8f70a6a8ef7ae33b87e3566a33c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-09-26 14:30:26 -0500

    init

---
+++ custom-fx/src/pipeline.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#xtract -bpx <bedpostX folder> -species HUMAN -out <output folder> -stdwarp ./bedpostx.bedpostX/xfms/standard2diff.mat ./bedpostx.bedpostX/xfms/diff2standard.mat -p <path to custom protocol folder> -str <path to customStructureList.txt>
+
